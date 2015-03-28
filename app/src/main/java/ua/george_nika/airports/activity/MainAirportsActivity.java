0af8c0d34a75e10f67210b0134a774606044e4f1
George <George_nika@mail.ru>
2015-03-28 14:06:44 +0200

    Drawer Layout on main screen with  button

--- app/src/main/java/ua/george_nika/airports/activity/MainAirportsActivity.java
+++ app/src/main/java/ua/george_nika/airports/activity/MainAirportsActivity.java
@@ -1,8 +1,15 @@
 package ua.george_nika.airports.activity;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
@@ -15,14 +22,19 @@ import ua.george_nika.airports.database.FactoryDb;
 import ua.george_nika.airports.util.AirportAutoCompleteAdapter;
 
 
-public class MainAirportsActivity extends Activity {
+public class MainAirportsActivity extends ActionBarActivity {
 
     private AutoCompleteTextView airportTitle;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
+
+    CharSequence mTitle,mDrawerTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2_airports);
+        setContentView(R.layout.activity_main_airports);
         initializeVariable();
         setAdapter();
         setListeners();
@@ -30,6 +42,9 @@ public class MainAirportsActivity extends Activity {
 
     private void initializeVariable(){
         airportTitle = (AutoCompleteTextView) findViewById(R.id.text_find_airport);
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        mTitle = mDrawerTitle = getTitle();
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
     }
 
     private void setAdapter(){
@@ -54,7 +69,7 @@ public class MainAirportsActivity extends Activity {
         buttonHttp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // startActivity(new Intent(v.getContext(), MapActivity.class));
+              //  startActivity(new Intent(v.getContext(), MapActivity.class));
             }
         });
 
@@ -80,6 +95,69 @@ public class MainAirportsActivity extends Activity {
             }
         });
 
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                toolbar,
+                R.string.main_drawer_open,  /* "open drawer" description for accessibility */
+                R.string.main_drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view)
+            {
+                super.onDrawerClosed(view);
+                //invalidateOptionsMenu();
+                syncState();
+            }
+
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+                //invalidateOptionsMenu();
+                syncState();
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //Set the custom toolbar
+        if (toolbar != null){
+            setSupportActionBar(toolbar);
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        actionBarDrawerToggle.syncState();
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
     }
 
 
